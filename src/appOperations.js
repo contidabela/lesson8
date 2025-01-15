1cac0acaf14bf4448a24e4309a307a7979adcb49
David Dabic <david.dabic@continental-corporation.com>
2025-01-15 15:29:27 +0100

    added a new function

--- src/appOperations.js
+++ src/appOperations.js
@@ -1,4 +1,11 @@
 function multiply(num1, num2){
     return num1*num2;
 }
-module.exports = multiply;
+
+function add(num1, num2){
+    return num1+num2;
+}
+module.exports = {
+    multiply,
+    add
+}
