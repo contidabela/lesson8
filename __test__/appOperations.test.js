0545079805f88984f82daba926ff1f61694e818a
David Dabic <david.dabic@continental-corporation.com>
2025-01-17 11:56:45 +0100

    added divide feature

--- __test__/appOperations.test.js
+++ __test__/appOperations.test.js
@@ -2,12 +2,16 @@ const appOperations = require('../src/appOperations');
 
 test("multiplication property of zero", ()=> {
     expect(appOperations.multiply(5,0)).toBe(0);
-})
+});
 
 test("adding two values", ()=> {
     expect(appOperations.add(5,5)).toBe(10);
-})
+});
 
 test("substracting two values", ()=> {
     expect(appOperations.substract(7,5)).toBe(2);
-})
+});
+
+test("dividing two values", ()=> {
+    expect(appOperations.divide(10,5)).toBe(2);
+});
