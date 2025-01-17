5ae0c5e87bfd09a8fe9fc674ba3aefb4fcae9150
David Dabic <david.dabic@continental-corporation.com>
2025-01-17 11:56:45 +0100

    added divide feature

--- src/appOperations.js
+++ src/appOperations.js
@@ -9,8 +9,13 @@ function add(num1, num2){
 function substract(num1, num2){
     return num1-num2;
 }
+
+function divide(num1, num2){
+    return num1/num2;
+}
 module.exports = {
     multiply,
     add,
-    substract
+    substract,
+    divide
 }
